8045b4d11ee8ff9a4a58e9274535d079fd02c7a8
Aqshola <aqsholafid2@gmail.com>
2021-05-31 20:24:06 +0700

    setup theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FA4A0C',
+        gray: {
+          100: '#F5F5F8',
+          200: '#EEEEEE',
+          300: '#F6F6F9',
+        },
+      },
+      fontFamily: {
+        display: ['Open Sans', ...defaultTheme.fontFamily.sans],
+        body: ['Open Sans', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   variants: {
     extend: {},
