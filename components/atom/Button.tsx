0b285f7bbe49124c7477fc81e14aff9615310613
Aqshola <aqsholafid2@gmail.com>
2021-06-01 21:54:08 +0700

    create relative path and fix structure folder

---
+++ components/atom/Button.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from 'react';
+import { motion } from 'framer-motion';
+
+interface Props {
+  children?: ReactNode;
+  onClick?: (params: any) => any;
+  width?: 'w-full' | 'w-10' | 'w-32';
+}
+
+export default function Button({ children, onClick, width = 'w-10' }: Props) {
+  return (
+    <motion.button
+      className="bg-primary py-2 px-10 text-white rounded-full text-sm"
+      onClick={onClick}
+    >
+      {children}
+    </motion.button>
+  );
+}
