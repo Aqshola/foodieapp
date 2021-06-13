bc19e68ab22d1a22642e1abf561bf43f01ce2de2
Aqshola <aqsholafid2@gmail.com>
2021-06-13 13:30:12 +0700

    prepare slider anim

--- components/molecule/Card.tsx
+++ components/molecule/Card.tsx
@@ -1,7 +1,23 @@
 import Image from 'next/image';
+import { motion } from 'framer-motion';
 export default function Card(params) {
+  const Variant = {
+    visible: {
+      x: 0,
+      scale: 1,
+    },
+    hidden: {
+      scale: 0.7,
+      x: '100%',
+    },
+  };
   return (
-    <div className="bg-white w-44 h-52 mr-5 rounded-xl shadow-card flex flex-col items-center justify-center relative sm:mt-10">
+    <motion.div
+      variants={Variant}
+      initial="hidden"
+      animate="visible"
+      className="bg-white w-44 h-52 mr-5 rounded-xl shadow-card flex flex-col items-center justify-center relative sm:mt-10"
+    >
       <div className="absolute -top-14">
         <Image src="/food1.png" width="164px" height="164px" />
       </div>
@@ -9,6 +25,6 @@ export default function Card(params) {
         Veggie <br /> tomato Mix
       </h1>
       <p className="text-red-100 font-semibold mt-3">N1,900</p>
-    </div>
+    </motion.div>
   );
 }
